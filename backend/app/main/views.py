0509e8d169a4891cab14725e63f2c40df6873444
m.sergeenkov <m.sergeenkov@int.unitia.cloud>
2019-04-28 21:53:32 +0300

    Реализован запрос для получения трудозатрат по воркам для вывода таблицы

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -1,30 +1,47 @@
-from flask import render_template, session, request, redirect, url_for, abort, jsonify
+from flask import render_template, session, request, redirect, url_for, abort, jsonify, json
 from app.main import main
 from app.main.models import User, Issue
 from app.main.jira import get_projects, import_issues
+from datetime import datetime
 
 
 @main.route('/', methods=['GET'])
 def index():
-	project_list = get_projects()
+    project_list = get_projects()
 
-	return render_template("main/index.html", project_list=project_list)
+    print(list(Issue.group_by_work_type()))
+
+    return render_template("main/index.html", project_list=project_list)
+
+
+@main.route('/hours_per_work_type_table', methods=['GET'])
+def get_hours_per_work_type_table():
+    start_date = datetime.strptime(request.args.get('start_date'), '%Y-%m-%d')
+    end_date = datetime.strptime(request.args.get('end_date'), '%Y-%m-%d')
+
+    try:
+        duration = request.args.get('duration')
+    except KeyError:
+        duration = 'month'
+
+    return jsonify(Issue.hours_per_work_type_table(start_datetime=start_date, end_datetime=end_date,
+                                                   duration=duration))
 
 
 @main.route('/', methods=['POST'])
 def show_efficiency():
-	options = {
-		'component': request.form['component'],
-		'first_date': request.form['first_date'],
-		'second_date': request.form['second_date']}
+    options = {
+        'component': request.form['component'],
+        'first_date': request.form['first_date'],
+        'second_date': request.form['second_date']}
 
-	issue_list = Issue(component=options['component']).find_by_component()
+    issue_list = Issue(component=options['component']).find_by_component()
 
-	return render_template("main/efficiency.html", issue_list=issue_list)
+    return render_template("main/efficiency.html", issue_list=issue_list)
 
 
 @main.route('/import/', methods=['GET'])
 def import_jira_issues():
-	import_issues()
+    import_issues()
 
-	return redirect(url_for('.index'))
+    return redirect(url_for('.index'))
