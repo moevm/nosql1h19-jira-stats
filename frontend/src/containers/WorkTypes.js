078880682e8eef6e314033fb03db282059c26151
Leonid Kutsenok <leo@sochi.com>
2019-04-28 15:38:57 +0300

    Добавлена таблица распределения трудозатрат по неделям

--- frontend/src/containers/WorkTypes.js
+++ frontend/src/containers/WorkTypes.js
@@ -1,31 +1,52 @@
 import React, {Component} from 'react';
-import {Bar, Doughnut, Line, Pie, Polar, Radar} from 'react-chartjs-2';
-import {Card, CardBody, CardColumns, CardHeader, FormGroup, Label, Input} from 'reactstrap';
+import {Bar} from 'react-chartjs-2';
+import {Card, CardBody, CardHeader, FormGroup, Label, Input, Button} from 'reactstrap';
 import {CustomTooltips} from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import ReactTable from "react-table";
+import _ from 'lodash'
+import 'react-table/react-table.css'
+
+const data = [
+    {
+        workType: 'Разработка', work: 'WORK-1', hours: {
+            '01 2019': 10,
+            '02 2019': 20,
+            '03 2019': 10,
+            '04 2019': 10,
+            '05 2019': 10,
+            '06 2019': 10,
+            '07 2019': 10,
+            '08 2019': 10,
+        }
+    },
+    {
+        workType: 'Разработка', work: 'WORK-2', hours: {
+            '01 2019': 11,
+            '02 2019': 21,
+            '03 2019': 11,
+        }
+    }
+];
+
+const columns = [
+    {
+        Header: 'Направление',
+        accessor: 'workType',
+    }, {
+        Header: 'Проект',
+        accessor: 'work',
+        Aggregated: (row) => <span>{row.value}</span>
+    }, ...Object.keys(data[0].hours).map((week) => ({
+        Header: week,
+        id: week,
+        accessor: (row) => row.hours[week],
+        aggregate: vals => _.sum(vals)
+    }))];
 
 const bar = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    // datasets: [
-    //     {
-    //         label: 'Разработка',
-    //         // backgroundColor: 'rgba(75,192,192,0.6)',
-    //         // borderColor: 'rgb(75,192,192)',
-    //         // borderWidth: 1,
-    //         // hoverBackgroundColor: 'rgba(75,192,192,0.8)',
-    //         // hoverBorderColor: 'rgba(75,192,192,1)',
-    //         data: [55, 43, 17, 98, 1, 77, 42],
-    //     },
-    //     {
-    //         label: 'Дизайн',
-    //         // backgroundColor: 'rgba(255, 99, 132,0.6)',
-    //         // borderColor: 'rgba(255, 99, 132,1)',
-    //         // borderWidth: 1,
-    //         // hoverBackgroundColor: 'rgba(255, 99, 132,0.8)',
-    //         // hoverBorderColor: 'rgba(255, 99, 132,1)',
-    //         data: [65, 59, 80, 81, 56, 55, 40],
-    //     },
-    // ],
 };
+
 const options = {
     tooltips: {
         enabled: false,
@@ -103,7 +124,7 @@ export default class WorkTypes extends Component {
                             <CardHeader>
                                 Параметры
                             </CardHeader>
-                            <CardBody style={{height: 340}}>
+                            <CardBody>
                                 <FormGroup>
                                     <Label htmlFor="date_start">Начало периода</Label>
                                     <Input name="date_start" type="date"/>
@@ -127,6 +148,9 @@ export default class WorkTypes extends Component {
                                         <option value="design">Дизайн</option>
                                     </Input>
                                 </FormGroup>
+                                <div className="form-actions">
+                                    <Button type="submit" color="primary" style={{width: '100%'}}>Обновить</Button>
+                                </div>
                             </CardBody>
                         </Card>
                     </div>
@@ -136,13 +160,10 @@ export default class WorkTypes extends Component {
                                 График распределения трудозатрат по направлениям
                             </CardHeader>
                             <CardBody>
-                                <div className="chart-wrapper" style={{height: 300}}>
+                                <div className="chart-wrapper" style={{height: 355}}>
                                     <Bar data={{
                                         ...bar,
-                                        datasets: this.state.datasets.map((dataset, i) => {
-                                            console.log({...dataset, ...colors[i]});
-                                            return {...dataset, ...colors[i]}
-                                        })
+                                        datasets: this.state.datasets.map((dataset, i) => ({...dataset, ...colors[i]}))
                                     }} options={bar_options} redraw={true}/>
                                 </div>
                             </CardBody>
@@ -151,18 +172,17 @@ export default class WorkTypes extends Component {
                 </div>
                 <Card>
                     <CardHeader>
-                        График распределения трудозатрат по направлениям
+                        Распределение трудозатрат по направлениям
                     </CardHeader>
                     <CardBody>
-                        <div className="chart-wrapper" style={{height: 300}}>
-                            <Bar data={{
-                                ...bar,
-                                datasets: this.state.datasets.map((dataset, i) => {
-                                    console.log({...dataset, ...colors[i]});
-                                    return {...dataset, ...colors[i]}
-                                })
-                            }} options={bar_options} redraw={true}/>
-                        </div>
+                        <ReactTable
+                            data={data}
+                            pivotBy={['workType']}
+                            columns={columns}
+                            className="-highlight -striped"
+                            showPagination={false}
+                            minRows={0}
+                        />
                     </CardBody>
                 </Card>
             </div>
